f1cea7bcd498bc1256c612743aa856cf0f973d40
Tiago Scolari <tscolari@gmail.com>
2014-11-18 23:30:28 +0000

    Add a simple docode.runner object

---
+++ packages/docode/runner.go
@@ -0,0 +1,41 @@
+package docode
+
+import (
+	"../dockerwrapper"
+	"../docodeconfig"
+)
+
+type DocodeRunner interface{}
+
+type Runner struct {
+	config docodeconfig.Configuration
+	docker dockerwrapper.DockerWrapper
+}
+
+func NewWithWrapper(config docodeconfig.Configuration, docker dockerwrapper.DockerWrapper) *Runner {
+	return &Runner{
+		config: config,
+		docker: docker,
+	}
+}
+
+func New(config docodeconfig.Configuration) *Runner {
+	return &Runner{
+		config: config,
+		docker: dockerwrapper.New(),
+	}
+}
+
+func (r *Runner) Run() error {
+	err := r.docker.PullImage(r.config.ImageName, r.config.ImageTag)
+	if err != nil {
+		return err
+	}
+
+	return r.docker.Run(
+		r.config.RunList,
+		r.config.Ports,
+		r.config.ImageName,
+		r.config.ImageTag,
+	)
+}
---
+++ packages/docode/runner_test.go
@@ -0,0 +1,58 @@
+package docode_test
+
+import (
+	. "../docode"
+	"../docodeconfig"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+type fakeDockerWrapper struct {
+	image        string
+	tag          string
+	runList      []string
+	portMappings map[int]int
+}
+
+func (w *fakeDockerWrapper) PullImage(image, tag string) error {
+	w.image = image
+	w.tag = tag
+	return nil
+}
+
+func (w *fakeDockerWrapper) Run(runList []string, portMappings map[int]int, image, tag string) error {
+	w.image = image
+	w.tag = tag
+	w.runList = runList
+	w.portMappings = portMappings
+	return nil
+}
+
+var _ = Describe("runner", func() {
+	var wrapper *fakeDockerWrapper
+	var runner *Runner
+	var config docodeconfig.Configuration
+
+	Describe(".Run", func() {
+		JustBeforeEach(func() {
+			wrapper = &fakeDockerWrapper{}
+			config = docodeconfig.Configuration{
+				ImageName: "busybox",
+				ImageTag:  "oldone",
+				RunList:   []string{"ls", "cd tmp"},
+				Ports:     map[int]int{2222: 1111},
+			}
+
+			runner = NewWithWrapper(config, wrapper)
+		})
+
+		It("sends commands to the docker wrapper", func() {
+			runner.Run()
+			Expect(wrapper.image).To(Equal("busybox"))
+			Expect(wrapper.tag).To(Equal("oldone"))
+			Expect(wrapper.runList).To(Equal([]string{"ls", "cd tmp"}))
+			Expect(wrapper.portMappings).To(Equal(map[int]int{2222: 1111}))
+		})
+	})
+})
