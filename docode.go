c434bbe48ee9c5067abedc31239db2f853ac501f
Tiago Scolari <tscolari@gmail.com>
2015-01-06 23:50:19 -0200

    Add `-k` argument to set SSH-KEY on cli
    Also renames the `-config` argument to `-c`

--- docode.go
+++ docode.go
@@ -8,13 +8,23 @@ import (
 )
 
 func main() {
-	docodeFilePath := flag.String("config", "./DocodeFile", "ConfigFile to load")
+	docodeFilePath := flag.String("c", "./DocodeFile", "ConfigFile to load")
+	argsConfig := fetchConfigFromArgs()
+
 	flag.Parse()
 
 	fileConfig := docodeconfig.NewFromFile(*docodeFilePath)
-	runner := docode.New(fileConfig)
+	config := docodeconfig.MergeConfigurations(argsConfig, fileConfig)
+	runner := docode.New(config)
 	err := runner.Run()
 	if err != nil {
 		panic("ERROR: " + err.Error())
 	}
+}
+
+func fetchConfigFromArgs() docodeconfig.ArgsConfiguration {
+	argsConfig := docodeconfig.ArgsConfiguration{}
+	argsConfig.SSHKey = flag.String("k", "", "Ssh key path to use")
+
+	return argsConfig
 }
